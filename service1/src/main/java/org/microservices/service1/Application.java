b366fed15ee93d1f96e289a9aaa329828079c365
Henrik Larsson <reg@henkelarsson.se>
2017-09-16 13:38:16 +0200

    Fix service discovery and add service2 skeleton

--- service1/src/main/java/org/microservices/service1/Application.java
+++ service1/src/main/java/org/microservices/service1/Application.java
@@ -3,11 +3,13 @@ package org.microservices.service1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by henke on 2017-09-09.
  */
 @SpringBootApplication
+@EnableEurekaClient
 @RefreshScope
 public class Application {
     public static void main(String[] args) {
